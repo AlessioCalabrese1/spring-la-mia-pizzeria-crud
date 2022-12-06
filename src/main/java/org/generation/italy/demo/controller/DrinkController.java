bcaf7179089b412438e40c62418fffb1fbb2cabd
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-12-06 16:39:22 +0100

    errors

--- src/main/java/org/generation/italy/demo/controller/DrinkController.java
+++ src/main/java/org/generation/italy/demo/controller/DrinkController.java
@@ -9,10 +9,12 @@ import org.generation.italy.demo.serv.DrinkServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
 
@@ -39,8 +41,19 @@ public class DrinkController {
 	}
 	
 	@PostMapping("store")
-	public String store(@Valid Drink drink) {
+	public String store(@Valid Drink drink, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 		
+		if (bindingResult.hasErrors()) {
+			
+			System.err.println("ERROR ------------------------------------------");
+			System.err.println(bindingResult.getAllErrors());
+			System.err.println("------------------------------------------------");
+			
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			
+			return "redirect:/drink/create";
+		}
+
 		drinkServ.save(drink);
 		
 		return "redirect:/drink";
